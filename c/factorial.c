3051bd2d51af2b2c6cbad9928aca0c8d29fb78f0
Burhan <35541515+burhannn@users.noreply.github.com>
2019-10-03 12:09:51 +0200

    Added factorial in c

---
+++ c/factorial.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+int main()
+{
+  int i, n, fact = 1;
+ 
+  printf("Enter a number to calculate its factorial\n");
+  scanf("%d", &n);
+
+  for (i = 1; i <= n; c++){
+      fact = fact * i;
+  }
+ 
+  printf("Factorial of %d = %d\n", n, fact);
+ 
+  return 0;
+}
