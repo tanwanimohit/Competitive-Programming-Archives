62d6ea80f256649c88d52ec6d4ac1df285f7ef5f
Katheryn Osmond <katheryn.osmond@excella.com>
2019-10-10 08:00:53 -0400

    add anagram algorithm

---
+++ Java/Anagram.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter word one: ");
+        String word1 = scanner.nextLine();
+        word1 = word1.toLowerCase();
+        System.out.println("Enter word two: ");
+        String word2 = scanner.nextLine();
+        word2 = word2.toLowerCase();
+
+        char charArray1[] = word1.toCharArray();
+        char charArray2[] = word2.toCharArray();
+
+        Arrays.sort(charArray1);
+        Arrays.sort(charArray2);
+
+        String ordered1 = new String(charArray1);
+        String ordered2 = new String(charArray2);
+
+        if (ordered1.equals(ordered2)) {
+            System.out.println(word1 + " and " + word2 + " are anagrams");
+        }
+        else {
+            System.out.println(word1 + " and " + word2 + " are NOT anagrams");
+        }
+    }
+}
