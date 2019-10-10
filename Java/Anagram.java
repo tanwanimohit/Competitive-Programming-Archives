9e4a704f1c751ba83fffce570149565f11ac1306
Katheryn Osmond <katheryn.osmond@excella.com>
2019-10-10 08:03:16 -0400

    rename class

--- Java/Anagram.java
+++ Java/Anagram.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Main
+public class Anagram
 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
